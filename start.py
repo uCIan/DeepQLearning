17fb5f691c97f34c4a9e521a4b91b0ed5fa60030
uCIan <highlifegamer36@gmail.com>
2017-01-12 10:01:32 -0600

    Update start.py

--- start.py
+++ start.py
@@ -6,3 +6,10 @@ def main():
         agent = World.player
         max_act, max_val = max_Q(agent)
         (agent, a, r, agent_updated) = do_action(max_act)
+
+        #update Q
+        max_act, max_val = max_Q(agent_updated)
+        
+        #Start game
+        World.start_game()
+        print 'Success! Score: ', max_val
